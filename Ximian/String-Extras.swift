491de4f77b600d9c3c92634da7639f825db1a7f2
Daniel Gutiérrez <dan.gutierrez@gmail.com>
2023-05-01 18:14:22 -0400

    Upgrade to Swift 5 and fixed "Artist" and "Track Number" fields

--- Ximian/String-Extras.swift
+++ Ximian/String-Extras.swift
@@ -13,7 +13,7 @@ extension String {
 		var pos = -1
 		if let range = range(of:sub) {
 			if !range.isEmpty {
-				pos = characters.distance(from:startIndex, to:range.lowerBound)
+				pos = distance(from:startIndex, to:range.lowerBound)
 			}
 		}
 		return pos
@@ -22,16 +22,16 @@ extension String {
 	func subString(start:Int, length:Int = -1)->String {
 		var len = length
 		if len == -1 {
-			len = characters.count - start
+			len = count - start
 		}
-		let st = characters.index(startIndex, offsetBy:start)
-		let en = characters.index(st, offsetBy:len)
+		let st = index(startIndex, offsetBy:start)
+		let en = index(st, offsetBy:len)
 		let range = st ..< en
 		return substring(with:range)
 	}
 
 	func range()->Range<String.Index> {
-		return Range<String.Index>(startIndex ..< endIndex)
+        return self.startIndex ..< endIndex
 	}
 }
 
